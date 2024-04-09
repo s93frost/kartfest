3b40af65c27a707c19062d76424c0ff30bceabdc
s93frost <47425976+s93frost@users.noreply.github.com>
2024-04-09 16:06:38 +0100

    commented out unused imports to clear errors

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-import pandas as pd
 import sqlite3
+import pandas as pd
 
-from cs50 import SQL
-from flask import Flask, redirect, render_template, request, session
-from flask_session import Session
+# from cs50 import SQL
+from flask import Flask, render_template, request
+# from flask import Flask, redirect, session
+# from flask_session import Session
 # from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import (
